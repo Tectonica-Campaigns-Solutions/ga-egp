b2f06bbfce20f996850513b6ce26ace1b3f9d98f
Rodrigo Leon <rodrigo@tectonica.co>
2023-01-24 10:45:37 -0300

    EGP Map added.

--- src/components/Global/FilterMembers/FilterMembers.js
+++ src/components/Global/FilterMembers/FilterMembers.js
@@ -1,33 +1,43 @@
-import React from 'react'
-import Link from '../Link'
+import React, { useState } from 'react';
+import { navigate } from 'gatsby';
+import Link from '../Link';
+import EGPMap from '../EGPMap/EGPMap';
 
 function FilterMembers({ members, introduction }) {
+  const handleOnClickCountry = (e) => {
+    const countryId = e.target?.id;
+
+    if (!countryId) {
+      console.warn('No country found.');
+      return;
+    }
+
+    const currentUrl = window.location.pathname;
+
+    // Navigate to country page
+    navigate(currentUrl + countryId);
+  };
+
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col-lg-4">
-
-          <div
-            dangerouslySetInnerHTML={{__html: introduction}}
-          />
-          {
-            members.map(item => {
-              return (
-                <div>
-                  <Link to={item.node.slug}>{item.node.title}</Link>
-                </div>  
-              )
-            })
-          }
+          <div dangerouslySetInnerHTML={{ __html: introduction }} />
+          {members.map((item) => {
+            return (
+              <div>
+                <Link to={item.node.slug}>{item.node.title}</Link>
+              </div>
+            );
+          })}
         </div>
+
         <div className="col-lg-8">
-          Map goes here
+          <EGPMap handleOnClickCountry={handleOnClickCountry} />
         </div>
       </div>
-
-      
     </div>
-  )
+  );
 }
 
-export default FilterMembers
+export default FilterMembers;
