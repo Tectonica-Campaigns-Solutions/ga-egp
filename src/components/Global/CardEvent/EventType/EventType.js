3d2742004b3f5b02e098bfcf9d0186f112d3fbaa
Rodrigo Leon <rodrigo@tectonica.co>
2023-02-01 13:50:42 -0300

    icon optional

--- src/components/Global/CardEvent/EventType/EventType.js
+++ src/components/Global/CardEvent/EventType/EventType.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.scss';
 
 const EventType = ({ type }) => {
-  const { icon, title } = type;
+  const { icon = null, title } = type;
   return (
     <span className="event-type">
       {icon?.url && <img src={icon?.url} alt="Event type icon" />}
