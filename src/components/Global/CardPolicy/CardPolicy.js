d276af501cb4df482a0de81f2cc3f577074d901d
Rodrigo Leon <rodrigo@tectonica.co>
2023-02-02 10:45:33 -0300

    Policy papers and resolutions card fixes

--- src/components/Global/CardPolicy/CardPolicy.js
+++ src/components/Global/CardPolicy/CardPolicy.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import InformationCard from '../InformationCard/InformationCard';
+import { removeUnderscoreFromString } from '../../../utils';
 
-function CardPolicy({ title, intro, documents, model }) {
+function CardPolicy({ item }) {
+  const { title, documents, intro, model = null } = item;
+
+  const isPolicyPaper = model ? model.apiKey === 'policy_paper' : false;
+  const policyType = isPolicyPaper ? removeUnderscoreFromString(model?.apiKey) : model?.apiKey;
+
+  // TODO: Add correct slug or use path to model util
+  const url = isPolicyPaper ? null : 'slug';
+
   return (
     <InformationCard
       preTitle={
         <>
-          Adopted: <strong>Madrid, 10-12 May 2013</strong>
+          {isPolicyPaper ? (
+            <>
+              Adopted: <strong>Madrid, 10-12 May 2013</strong>
+            </>
+          ) : (
+            <>
+              Council: <strong>Copenhagen Congress 2022</strong>
+            </>
+          )}
         </>
       }
+      tag={policyType}
       title={title}
       intro={intro}
       documents={documents}
+      url={url}
     />
   );
 }
--- src/components/Global/InformationCard/InformationCard.js
+++ src/components/Global/InformationCard/InformationCard.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { isArray } from '../../../utils';
 import Document from '../Document/Document';
+import Link from '../Link';
+import Tag from '../Tag/Tag';
 
 import './index.scss';
 
-const InformationCard = ({ preTitle, title, intro, documentKey = 'Related Downloads', documents = [] }) => {
+const InformationCard = ({
+  tag,
+  preTitle,
+  title,
+  intro,
+  documentKey = 'Related Downloads',
+  documents = [],
+  url = null,
+}) => {
   return (
     <div className="information-card">
-      <div className="pre-title">{preTitle}</div>
+      <div className="header-information">
+        {tag && <Tag title={tag} />}
+        <div className="pre-title">{preTitle}</div>
+      </div>
 
-      <h6>{title}</h6>
+      <Link to={url} className={`${url ? 'active' : 'disable'}`}>
+        <h6>{title}</h6>
+      </Link>
+
       <div className="description" dangerouslySetInnerHTML={{ __html: intro }} />
 
       {isArray(documents) && (
