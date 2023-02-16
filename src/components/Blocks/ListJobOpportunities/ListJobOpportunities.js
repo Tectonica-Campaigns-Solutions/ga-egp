dd38c85fe929e1c0ac738ac6b3f874f4cb0567a9
Rodrigo Leon <rodrigo@tectonica.co>
2023-02-16 13:41:03 -0300

    List job opportunity

---
+++ src/components/Blocks/ListJobOpportunities/ListJobOpportunities.js
@@ -0,0 +1,37 @@
+import { useStaticQuery } from 'gatsby';
+import { graphql } from 'gatsby';
+import React from 'react';
+import { isArray } from '../../../utils';
+import JobOpportunityCard from '../../Global/JobOpportunityCard/JobOpportunityCard';
+
+const ListJobOpportunities = ({}) => {
+  const {
+    allDatoCmsJobOpportunity: { nodes: jobs },
+  } = useStaticQuery(graphql`
+    query {
+      allDatoCmsJobOpportunity {
+        nodes {
+          id
+          title
+          location
+          description
+          isRemote
+        }
+      }
+    }
+  `);
+
+  if (!isArray(jobs)) {
+    return null;
+  }
+
+  return (
+    <div className="container pt-5">
+      {jobs.map((job) => (
+        <JobOpportunityCard job={job} key={job.id} />
+      ))}
+    </div>
+  );
+};
+
+export default ListJobOpportunities;
