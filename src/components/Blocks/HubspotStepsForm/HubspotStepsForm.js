94f483b8138f9d9030030cdeb4b569d9c2d044b7
marcelsafont <marcel.safont1983@gmail.com>
2023-05-23 10:54:38 +0200

    test netlify form

--- src/components/Blocks/HubspotStepsForm/HubspotStepsForm.js
+++ src/components/Blocks/HubspotStepsForm/HubspotStepsForm.js
@@ -3,25 +3,34 @@ import React from 'react';
 function HubspotStepsForm({ block }) {
   return (
     <div className="container">
+      <form name="contact" method="POST" data-netlify="true" action="/">
         {
           block.steps.map( item =>{
             return (
               <div className="row mt-5">
                 <div className="col">{item.description}</div>
                 <div className="col">
-                  {
-                    item.formFields.map( el => {
-                      return (
-                        <div>{ el.label }</div>
-                
-                      )
-                    })
-                  }
+                  
+                    {
+                      item.formFields.map( el => {
+                        return (
+                          <div>
+                            <label>{ el.label }</label>
+                            <input type="text" name="name"></input>
+                          </div>
+                        )
+                      })
+                    }
+                    
+                  
+                  
                 </div>
               </div>
             )
           })
         }
+        <button type="submit">Send</button>
+        </form>
     </div>
   );
 }
