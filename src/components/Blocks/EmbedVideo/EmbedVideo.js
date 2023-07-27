f05c45a715c4175a68203998fcf83b32b048791e
Rodrigo Leon <rodrigo@tectonica.co>
2023-07-27 09:04:50 -0300

    Embed video responsive added

--- src/components/Blocks/EmbedVideo/EmbedVideo.js
+++ src/components/Blocks/EmbedVideo/EmbedVideo.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './index.scss';
 
-function EmbedVideo({block}) {
+function EmbedVideo({ block }) {
   return (
-    <div className="container">
-      <iframe src={`//www.youtube.com/embed/${block.video.providerUid}?rel=0`} frameborder="0" allowfullscreen className="w-100"></iframe>
+    <div className="container embed-video">
+      <iframe
+        src={`//www.youtube.com/embed/${block.video.providerUid}?rel=0`}
+        frameborder="0"
+        allowfullscreen
+        className="w-100 video"
+      ></iframe>
     </div>
-  )
+  );
 }
 
-export default EmbedVideo
+export default EmbedVideo;
