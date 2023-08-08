ed0a2b4b9f56ae4278df2d77c5cbcb2f552d4281
Rodrigo Leon <rodrigo@tectonica.co>
2023-08-08 08:34:46 -0300

    Login: Preview cta block fixes on link styles

--- src/components/Blocks/PreviewCtaBlock/PreviewCtaBlock.js
+++ src/components/Blocks/PreviewCtaBlock/PreviewCtaBlock.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import Link from '../../Global/Link';
+import React, { useState } from 'react';
 import { isArray } from '../../../utils';
+import ActionButton from '../../Global/Button/ActionButton';
 
 import './index.scss';
+import { navigate } from 'gatsby';
+import RadioInput from '../../Global/Form/RadioInput';
 
 const PreviewCtaBlock = ({ block }) => {
+  const [selectedCta, setSelectedCta] = useState(null);
   const { title, description, backgroundImage, backgroundColor, ctas = [] } = block;
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (!selectedCta) return;
+    navigate(selectedCta);
+  };
 
   return (
     <div
@@ -21,13 +30,26 @@ const PreviewCtaBlock = ({ block }) => {
 
           {isArray(ctas) && (
             <div className="col-lg-4 offset-lg-2">
-              <div className="ctas">
-                {ctas.map((cta) => (
-                  <Link key={cta.id} to={cta.url.url}>
-                    <div dangerouslySetInnerHTML={{ __html: cta.description }} />
-                  </Link>
-                ))}
-              </div>
+              <form onSubmit={handleOnSubmit}>
+                <div className="ctas">
+                  {ctas.map((cta) => (
+                    <div className="input-cta">
+                      <input
+                        type="radio"
+                        id={cta.id}
+                        name="link_to"
+                        value={cta.url?.url}
+                        onChange={(event) => setSelectedCta(event.target.value)}
+                      />
+                      <label for={cta.id} dangerouslySetInnerHTML={{ __html: cta.description }} />
+                    </div>
+                  ))}
+                </div>
+
+                <div className="btn-submit-container">
+                  <ActionButton label="Continue" type="submit" />
+                </div>
+              </form>
             </div>
           )}
         </div>
