12ad614caf6847da1311daed477c00be8c1e05ab
Rodrigo Leon <rodrigo@tectonica.co>
2023-08-10 08:59:38 -0300

    Job list: fix text size

--- src/components/Blocks/ListJobOpportunities/ListJobOpportunities.js
+++ src/components/Blocks/ListJobOpportunities/ListJobOpportunities.js
@@ -26,7 +26,22 @@ const ListJobOpportunities = () => {
   `);
 
   if (!isArray(jobs)) {
-    return (<div className="container my-5"><h3>There are no positions at the moment.</h3></div>);
+    return (
+      <div className="container my-5">
+        <h3
+          style={{
+            fontFamily: 'Jaldi',
+            fontStyle: 'normal',
+            fontWeight: 800,
+            fontSize: '31px',
+            lineHeight: '110%',
+            color: 'var(--egp-darker-green)',
+          }}
+        >
+          There are no positions at the moment.
+        </h3>
+      </div>
+    );
   }
 
   return (
