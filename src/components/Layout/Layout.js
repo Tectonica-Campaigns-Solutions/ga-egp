94d7f5ae0ad3ae18ba51b68194ab9fd67103f466
marcelsafont <marcel@tectonica.co>
2023-09-27 10:48:08 +0200

    add hubspot tracking code

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import TopMessage from '../Global/TopMessage/TopMessage';
 import Header from '../Header';
@@ -7,6 +7,13 @@ import Footer from '../Global/Footer/Footer';
 import '../../styles/main.scss';
 
 const Layout = ({ navbarWhite = false, navbarYellowHover = false, location, children }) => {
+
+  useEffect(() => {
+    const trackingHubspot = document.createElement('script');
+    trackingHubspot.id = 'hs-script-loader';
+    trackingHubspot.src = 'https://js-eu1.hs-scripts.com/26289884.js';
+    document.body.appendChild(trackingHubspot);
+  })
   return (
     <>
       <TopMessage />
