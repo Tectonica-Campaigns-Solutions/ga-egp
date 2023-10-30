8a98e4ca26816364e8e31e61875f847dd3405c4b
Rodrigo Leon <rodrigo@tectonica.co>
2023-10-30 12:17:34 -0300

    fix

--- functions/checkout.js
+++ functions/checkout.js
@@ -10,7 +10,7 @@ exports.handler = async (event) => {
     mode: mode,
     // payment_method_types: ['card'],
     payment_method_types: isSubscriptionMode
-      ? ['card', 'bancontact', 'eps']
+      ? ['card', 'bancontact']
       : ['card', 'bancontact', 'p24', 'eps', 'giropay', 'sofort', 'ideal'],
     success_url: `${process.env.BASE_URL}/donation-thank-you`,
     cancel_url: process.env.BASE_URL,
